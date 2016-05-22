cc8c3c394f7dce598011c5a677252bac2915e19a
Joe Watkins <info@joe-watkins.io>
2016-05-22 09:58:00 -0700

    Add no cors

--- sw.js
+++ sw.js
@@ -47,7 +47,9 @@ self.addEventListener('fetch',function(event){
 	// console.log(event.request);
 	event.respondWith(
 		caches.match(event.request).then(function(response){
-			return response || fetch(event.request);
+			return response || fetch(event.request,{
+        mode: 'no-cors'
+      });
 		})
 	)
 });
