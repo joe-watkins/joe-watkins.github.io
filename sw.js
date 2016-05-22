2b5838f0c830334628258377b1d89c60f99c1a33
Joe Watkins <info@joe-watkins.io>
2016-05-22 10:26:42 -0700

    sw

--- sw.js
+++ sw.js
@@ -25,7 +25,11 @@ self.addEventListener("install", function(e){
         '/images/bg-city.jpg',
         'https://fonts.gstatic.com/s/opensans/v13/cJZKeOuBrn4kERxqtaUH3aCWcynf_cDxXwCLxiixG1c.ttf',
         'https://fonts.gstatic.com/s/opensans/v13/k3k702ZOKiLJc3WVjuplzInF5uFdDttMLvmWuJdhhgs.ttf',
-        '/webdev/progressive-enhancement-and-the-2016-presidential-race/'
+        '/webdev/progressive-enhancement-and-the-2016-presidential-race/',
+        '/webdev/migrate-from-jekyll-to-hugo/',
+        '/accessibility/infographics-may-not-belong-on-the-web/',
+        '/accessibility/top-people-to-follow-in-web-accessibility/'
+
 			]) // addAll()
 		}) // .open()
 	); // waitUntil
@@ -38,9 +42,7 @@ self.addEventListener('fetch',function(event){
 	// console.log(event.request);
 	event.respondWith(
 		caches.match(event.request).then(function(response){
-			return response || fetch(event.request,{
-        mode: 'no-cors'
-      });
+			return response || fetch(event.request);
 		})
 	)
 });
