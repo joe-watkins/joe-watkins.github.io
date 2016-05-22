62dd0e8c42fdf6e8dae2818d5d06b3dd5c7dbb3d
Joe Watkins <info@joe-watkins.io>
2016-05-22 12:03:04 -0700

    add ga gif

--- sw.js
+++ sw.js
@@ -34,7 +34,8 @@ self.addEventListener("install", function(e){
         'https://joe-watkins.io/javascript/organize-your-jquery-with-object-literal-notation/',
         'https://joe-watkins.io/css-visual-regression-testing-with-grunt-backstopjs/',
         'https://joe-watkins.io/accessibility/getting-started-with-grunt-tenon-client/',
-        'https://joe-watkins.io/accessibility/please-stop-using-placeholders-as-labels/'
+        'https://joe-watkins.io/accessibility/please-stop-using-placeholders-as-labels/',
+        'https://ssl.google-analytics.com/__utm.gif?utmwv=5.6.7&utms=21&utmn=732733281&utmhn=joe-watkins.io&utmcs=UTF-8&utmsr=1440x900&utmvp=1333x375&utmsc=24-bit&utmul=en-us&utmje=0&utmfl=21.0%20r0&utmdt=JoeWatkins.io&utmhid=1603467120&utmr=0&utmp=%2F&utmht=1463943709630&utmac=UA-857864-9&utmcc=__utma%3D4393640.294469123.1463943337.1463943337.1463943337.1%3B%2B__utmz%3D4393640.1463943337.1.1.utmcsr%3D(direct)%7Cutmccn%3D(direct)%7Cutmcmd%3D(none)%3B&utmjid=&utmu=qAAAAAAAAAAAAAAAAAAAAAAE~'
 			]) // addAll()
 		}) // .open()
 	); // waitUntil
