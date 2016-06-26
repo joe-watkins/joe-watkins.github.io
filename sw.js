99d639c6f20859422ff8630c153834a05bcbf0b4
Joe Watkins <info@joe-watkins.io>
2016-06-26 07:55:39 -0700

    service worker update

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // offline app
 // https://developers.google.com/web/fundamentals/getting-started/your-first-offline-web-app
 
-var serviceWorkerVersion = "v5"; // increment this value each update
+var serviceWorkerVersion = "static-v1"; // increment this value each update
 
 // add polyfill for caching
 importScripts('/cache-polyfill.js');
@@ -40,6 +40,7 @@ self.addEventListener("install", function(e){
         'https://joe-watkins.io/accessibility/please-stop-using-placeholders-as-labels/',
         'https://ssl.google-analytics.com/__utm.gif?utmwv=5.6.7&utms=21&utmn=732733281&utmhn=joe-watkins.io&utmcs=UTF-8&utmsr=1440x900&utmvp=1333x375&utmsc=24-bit&utmul=en-us&utmje=0&utmfl=21.0%20r0&utmdt=JoeWatkins.io&utmhid=1603467120&utmr=0&utmp=%2F&utmht=1463943709630&utmac=UA-857864-9&utmcc=__utma%3D4393640.294469123.1463943337.1463943337.1463943337.1%3B%2B__utmz%3D4393640.1463943337.1.1.utmcsr%3D(direct)%7Cutmccn%3D(direct)%7Cutmcmd%3D(none)%3B&utmjid=&utmu=qAAAAAAAAAAAAAAAAAAAAAAE~'
 			]) // addAll()
+
 		}) // .open()
 	); // waitUntil
 
